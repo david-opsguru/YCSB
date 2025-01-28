1255999d88eaa06ced0d24e9e6ce854cdd572b05
David Mizrahi <david@opsguru.co.il>
2025-01-28 17:52:11 +0200

    applied SHARDED type by default

--- ycsb-mongodb/mongodb/src/main/java/site/ycsb/db/MongoDbClient.java
+++ ycsb-mongodb/mongodb/src/main/java/site/ycsb/db/MongoDbClient.java
@@ -535,7 +535,7 @@ public class MongoDbClient extends DB {
                     .serverApi(serverApi)
                     .writeConcern(writeConcern)
                     .readPreference(readPreference)
-                    .applyToClusterSettings(builder -> builder.requiredClusterType(ClusterType.REPLICA_SET))
+                    .applyToClusterSettings(builder -> builder.requiredClusterType(ClusterType.SHARDED))
                     .build();
                 mongoClient = MongoClients.create(settings);
                 mongoDatabase = mongoClient.getDatabase(database);
